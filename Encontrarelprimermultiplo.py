99ec5a7a58319c539d1153e4c5cfc5c198914412
Tatiana2807 <gr0233032025@unab.edu.sv>
2025-03-04 15:14:57 -0500

    Guardar

---
+++ Encontrarelprimermultiplo.py
@@ -0,0 +1,6 @@
+numero = 101
+while True:
+    if numero % 7 == 0:
+        print(f"El primer multiplo de 7 en el rango de 100 a 200 es {numero}")
+        break
+        numero +=1
---
+++ Sumadedigitos.py
@@ -0,0 +1,11 @@
+numero = input("Ingrese un número: ")
+if numero.isdigit():
+    suma = 0
+    for in numero: 
+        suma += int(i)
+        prinr(f "La suma de los digitos de {numero} es {suma}")
+        else:
+            print("El valor ingresado no es un numero ")
+            print("Ingrese un número valido")
+            print("Fin del programa")
+
