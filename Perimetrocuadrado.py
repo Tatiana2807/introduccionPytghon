183e969d3a091e02655588c086df2bafb64eb687
Tatiana2807 <gr0233032025@unab.edu.sv>
2025-03-16 21:53:40 -0500

    cambio nuevo

---
+++ Perimetrocuadrado.py
@@ -0,0 +1,9 @@
+#perimetro del cuadrado (p) = lado (L) x 4.
+#Donde p es el perimetro y L es la longitud de uno 
+#de los lados del cuadrado 
+
+def perimetroCuadrado(lado):
+    p= lado * 4
+    print ("El perimetro es ", p , "unidades")
+
+perimetroCuadrado(23)
